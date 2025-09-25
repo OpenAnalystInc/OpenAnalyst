624fa9918ae4230beadddc8f4c4cd48e9d62f83c
Ayush <ayush.s@1to10x.com>
2025-09-25 12:44:22 +0530

    updated logo

--- src/esbuild.mjs
+++ src/esbuild.mjs
@@ -52,19 +52,19 @@ async function main() {
 			name: "copyFiles",
 			setup(build) {
 				build.onEnd(() => {
-					copyPaths(
-						[
-							["../README.md", "README.md"],
-							["../CHANGELOG.md", "CHANGELOG.md"],
-							["../LICENSE", "LICENSE"],
-							["../.env", ".env", { optional: true }],
-							["node_modules/vscode-material-icons/generated", "assets/vscode-material-icons"],
-							["../webview-ui/audio", "webview-ui/audio"],
-							["../webview-ui/src/components/oacode/assets/icons", "assets/icons"],
-						],
-						srcDir,
-						buildDir,
-					)
+						copyPaths(
+							[
+								["../README.md", "README.md"],
+								["../CHANGELOG.md", "CHANGELOG.md"],
+								["../LICENSE", "LICENSE"],
+								["../.env", ".env", { optional: true }],
+								["node_modules/vscode-material-icons/generated", "assets/vscode-material-icons"],
+								["../webview-ui/audio", "webview-ui/audio"],
+								// ["../webview-ui/src/components/oacode/assets/icons", "assets/icons"], // Removed to prevent overwriting src/assets/icons
+							],
+							srcDir,
+							buildDir,
+						)
 
 					// Copy walkthrough files to dist directory
 					copyPaths([["walkthrough", "walkthrough"]], srcDir, distDir)
